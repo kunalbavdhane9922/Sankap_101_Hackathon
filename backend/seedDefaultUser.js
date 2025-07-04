6ed9a871d60867a3a31b252625b6d8076d14d187
kunalbavdhane9922 <bavdhanekunal19@gmail.com>
2025-07-04 08:49:16 +0530

    Automated commit

--- backend/seedDefaultUser.js
+++ backend/seedDefaultUser.js
@@ -1,15 +1,13 @@
 const mongoose = require('./db');
 const User = require('./models/User');
-const bcrypt = require('bcryptjs');
 
 async function seed() {
   const username = 'demo';
   const password = 'demo123';
-  const hash = await bcrypt.hash(password, 10);
 
   let user = await User.findOne({ username });
   if (!user) {
-    user = new User({ username, password: hash });
+    user = new User({ username, password: password });
     await user.save();
     console.log('Default user created: demo');
   } else {
