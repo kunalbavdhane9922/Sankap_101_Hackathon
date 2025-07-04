6a2632c00a1d160d06e705a49d9378d77921152c
kunalbavdhane9922 <bavdhanekunal19@gmail.com>
2025-07-04 12:55:54 +0530

    Automated commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Home.css';
 import CompetitorComparison from "../components/CompetitorComparison";
-import CaptionGenerator from "../components/CaptionGenerator";
 import API_BASE_URL from "../config";
 
 const platformColors = {
@@ -162,8 +161,6 @@ const Home = () => {
               <AnalyticsCard key={account._id || account.platform + idx} account={account} analytics={analytics[account._id]} />
             ))}
           </div>
-          {/* AI Caption Generator */}
-          <CaptionGenerator />
           {/* Competitor Comparison Feature */}
           <CompetitorComparison niche="fitness" />
         </div>
